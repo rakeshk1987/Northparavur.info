9f6ed2253f0bd5556aedf3718a52e4dd7d4aa597
Rakesh Krishna <rakesh133@gmail.com>
2020-12-28 03:33:31 +0000

    added new grid items (news box and info box) to homepage

---
+++ scripts/index.js
@@ -0,0 +1,20 @@
+var updates = [
+    {
+        id="1",
+        category: "news",
+        date: "27/12/2020",
+        content: "This is a test news"
+    },
+    {
+        id="2",
+        category: "events",
+        date: "27/12/2020",
+        content: "This is a text event"
+    },
+    {
+        id="1",
+        category: "information",
+        date: "27/12/2020",
+        content: "This is a text information"
+    },
+];
