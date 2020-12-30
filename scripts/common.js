1515e56f89ab404b9b5f4a0a31874413e9fa8783
Rakesh Krishna <rakesh133@gmail.com>
2020-12-30 04:37:12 +0000

    Updated new header and footer to all the pages and created new personal page

--- scripts/common.js
+++ scripts/common.js
@@ -11,7 +11,9 @@ function respMenu() {
 
 
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {
+    myFunction()
+};
 
 // Get the navbar
 var navbar = document.getElementById("myTopnav");
@@ -27,3 +29,28 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+
+
+
+//footer content
+document.getElementById('footer').innerHTML = `
+
+<div>  
+    <footer class="footer">
+      <p id="copyright"></p>
+      <p style="font-size: small;">Content created for educational purpose!!</p>
+      <p style="font-size: x-small;">Last Updated on 29/12/2020</p>
+      <i class="fab fa-html5 circle-icon" style="color: white; font-size:30px;"></i>
+      <i class="fab fa-css3-alt circle-icon" style="color: white; font-size:30px;"></i>
+      <i class="fab fa-js circle-icon" style="color: white; font-size:30px;"></i>
+      <p style="font-size: small;"><a style="color: white; text-decoration: none;" href="https://github.com/rakeshk1987/Northparavur.info" target="_blank">visit github page</a></p>
+    </footer>
+  </div>
+
+`; 
+
+
+//update copyright year
+var year = new Date().getFullYear();
+document.getElementById('copyright').innerHTML = `Copyright &copy; ${year} by northparavur.info`;
---
+++ scripts/personals.js
@@ -0,0 +1,86 @@
+var personaldata = [
+    {
+        id: 1,
+        Name: "Kedamangalam Sadanandan",
+        Life: "1926-2008",
+        profession:"Kadhaprasangam (story telling)",
+        details:"Kedamangalam Sadanandan was a Kadhaprasangam artist, film actor, screenwriter and lyricist from Kerala. He performed Kadhaprasangam in more than 15,000 stages for over 50,000 hours in a career spanning about 64 years. He wrote scripts for 12 films, lyrics for more than hundred films and acted in about 40 films.",
+        photo_small:"../img/personals/kedamangalam.jpg",
+        photo_large:""
+    },
+    {
+        id: 2,
+        Name: "Sankaradi",
+        Life: "1924-2001",
+        profession:"Actor, Politician, Journalist",
+        details:"Chandrasekhara Menon, better known by his screen name Sankaradi (1924–2001), was an Indian actor. He was a veteran Malayalam comedian and character artist, who had over 700 Malayalam films to his credit. He was one of the first realistic actors in South Indian cinema.",
+        photo_small:"../img/personals/sankaradi.jpg",
+        photo_large:""
+    },
+    {
+        id: 3,
+        Name: "Kesari Balakrishna Pillai",
+        Life: "1889-1960",
+        profession:"Writer, Critic, Journalist",
+        details:"Akathoot Balakrishna Pillai, known as Kesari Balakrishna Pillai, was a Malayalam writer, art and literary critic and journalist, considered by many as one of the most influential thinkers of modern Kerala. He was the eponymous founder of the newspaper, Kesari and was one of the three major figures in modern Malayalam literary criticism, along with Joseph Mundassery and M. P. Paul.",
+        photo_small:"../img/personals/kesari.jpg",
+        photo_large:""
+    },
+    {
+        id: 4,
+        Name: "Paravoor T. K. Narayana Pillai",
+        Life: "1890–1971",
+        profession:"Politician",
+        details:"Paravoor T. K. Narayana Pillai was an Indian freedom fighter during the British Raj in India and was a member of the Indian National Congress (INC) which fought against the British rule in India. He was the last Prime Minister of Travancore and the first Chief Minister of Travancore-Cochin at the time of its formation in 1949. He was commonly known as Parur TK.",
+        photo_small:"../img/personals/paravurtk.jpg",
+        photo_large:""
+    },
+    {
+        id: 5,
+        Name: "Paravoor Bharathan",
+        Life: "1929-2015",
+        profession:"Actor",
+        details:"Paravoor Bharathan was an Indian actor who acted in Malayalam films. He started his career in movies during the 1950s. Bharathan is known as a versatile actor and has played a variety of roles including negative roles, character roles and comedy roles.",
+        photo_small:"../img/personals/bharathan.jpg",
+        photo_large:""
+    },
+    {
+        id: 6,
+        Name: "G. A. Menon",
+        Life: "1931-2003",
+        profession:"Entrepreneur",
+        details:"was a US-based entrepreneur born in North Paravur. He set up the two Technopark companies namely UST Global and Toonz Animation",
+        photo_small:"../img/personals/gamenon.jpg",
+        photo_large:""
+    },
+    {
+        id: 7,
+        Name: "Salim Kumar",
+        Life: "1969-",
+        profession:"Actor",
+        details:"Salim Kumar is an Indian film actor born in North Paravur, director and writer in Malayalam cinema. He won the National Film Award for Best Actor in 2010 for his role in Adaminte Makan Abu",
+        photo_small:"../img/personals/salim.jpg",
+        photo_large:""
+    },
+];
+
+function personaldataTmplt(person)
+{
+    return `
+    
+    <div class="personals">
+    
+        <a data-lightbox="myimage" data-title = "${person.Name}" href="${person.photo_small}"> <img class="place-photo" src="${person.photo_small}"></a>
+        <h4><b>${person.Name}</b></h4>
+        <p><b>Years:</b> ${person.Life}</P>
+        <p><b>Profession:</b> ${person.profession}</P>
+        <p><b>Details:</b> ${person.details}</P>
+        <br>
+        <button id="${person.id}" onclick="descOpen(this.id)">More Info</button>
+
+    </div>
+    
+    `
+}
+
+document.getElementById('personals').innerHTML = `${personaldata.map(personaldataTmplt).join('')}`
